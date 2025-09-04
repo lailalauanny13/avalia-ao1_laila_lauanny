f90615f6f572a8b71e7f65ac3816516bbb8b6e1b
rhavss <puupi169@gmail.com>
2025-09-04 09:35:38 -0300

    commit avaliaçao pratica-questoes resolvidas

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+n=int(input("digite um numero:"))
+if n % 5 ==0:
+ print("ele e mutiplo de 5") 
+else:
+  print("ele nao e mutiplo de 5") 
+if n >0:
+  print("o numero e positivo")
+elif n <0:
+  print("o numero e negatvo")
+else:
+  print("o numero e igual a 0")
+  
